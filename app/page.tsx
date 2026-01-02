adae59d4275e822e864214006d1f206edc825a14
ahmadalqwasmeh <133670059+ahmadalqwasmeh@users.noreply.github.com>
2026-01-03 01:57:32 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
-
 import { useEffect } from "react";
 
 export default function HomePage() {
   useEffect(() => {
     window.location.href = "/login";
   }, []);
-
   return null;
 }
